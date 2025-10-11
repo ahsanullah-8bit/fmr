62d7b88dbc57c94da905582a9cd02074f74d7ca7
Ahsan Ullah <hatrakhan@gmail.com>
2025-10-11 21:38:55 +0500

    cleanup: types

--- include/fmr/core/types.hpp
+++ include/fmr/core/types.hpp
@@ -4,28 +4,6 @@
 #include <opencv2/core/types.hpp>
 
 namespace fmr {
-    struct box_info {
-        cv::Rect box;
-        cv::Rect nms_box;
-        float conf;
-        int class_id;
-    };
-
-    struct obb_info {
-        cv::RotatedRect box;
-        cv::RotatedRect nms_box;
-        float conf;
-        int class_id;
-    };
-
-    struct pose_info {
-        cv::Rect box;
-        cv::Rect nms_box;
-        std::vector<cv::Point3f> keypoints;
-        float conf;
-        int class_id;
-    };
-
     struct keypoint {
         cv::Point2f point;
         float conf;
